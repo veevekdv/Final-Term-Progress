4986596b0d966900ba0b8a0caad705ddffc02786
Veevek Dave <veevekd401@gmail.com>
2024-04-24 18:29:40 -0400

    More endpoints - GET, PUT, DELETE
    - GET/:id : retrieves a particular to-do list by id
    - PUT: Updates an existing to-do list
    - DELETE: Deletes to-do list if exists
    - Error codes: 400, 500

--- To-Do List App/Back_End/routes/todoRoutes.js
+++ To-Do List App/Back_End/routes/todoRoutes.js
@@ -27,3 +27,37 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message });
     }
 });
+
+// Get a single todo by ID
+router.get('/:id', getTodo, (req, res) => {
+  res.json(res.todo);
+});
+
+// Update a todo by ID
+router.put('/:id', getTodo, async (req, res) => {
+  if (req.body.title != null) {
+      res.todo.title = req.body.title;
+  }
+  if (req.body.description != null) {
+      res.todo.description = req.body.description;
+  }
+  if (req.body.completed != null) {
+      res.todo.completed = req.body.completed;
+  }
+  try {
+      const updatedTodo = await res.todo.save();
+      res.json(updatedTodo);
+  } catch (err) {
+      res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a todo
+router.delete('/:id', getTodo, async (req, res) => {
+  try {
+      await res.todo.remove();
+      res.json({ message: 'Deleted Todo' });
+  } catch (err) {
+      res.status(500).json({ message: err.message });
+  }
+});
