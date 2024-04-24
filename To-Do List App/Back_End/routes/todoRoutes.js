e57925ce83be6a3a6e71086fa6e493cfcd7cb968
Veevek Dave <veevekd401@gmail.com>
2024-04-24 18:27:44 -0400

    Created Routes + Server
    POST and GET Requests
    - Posts: Adds a new to-do list item
    - Get: Retrieves all to-do list items

---
+++ To-Do List App/Back_End/routes/todoRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+// Get all todos
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a new todo
+router.post('/', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title,
+        description: req.body.description,
+        dueDate: req.body.dueDate
+    });
+
+    try {
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
