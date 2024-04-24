e8eeee4dc7159de40e1247829c984ba27ac71eb4
Veevek Dave <veevekd401@gmail.com>
2024-04-24 18:43:14 -0400

    Middleware
    loads a to-do item by ID and attaches it to the response object, making it available in the route handle

--- To-Do List App/Back_End/routes/todoRoutes.js
+++ To-Do List App/Back_End/routes/todoRoutes.js
@@ -61,3 +61,20 @@ router.delete('/:id', getTodo, async (req, res) => {
       res.status(500).json({ message: err.message });
   }
 });
+
+// Middleware to get todo by ID
+async function getTodo(req, res, next) {
+  let todo;
+  try {
+      todo = await Todo.findById(req.params.id);
+      if (todo == null) {
+          return res.status(404).json({ message: 'Cannot find todo' });
+      }
+  } catch (err) {
+      return res.status(500).json({ message: err.message });
+  }
+  res.todo = todo;
+  next();
+}
+
+module.exports = router;
